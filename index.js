2a9b19f4f9ce89c63fbe38d845bc001a48802519
Tom Harris <tom_3rd_2000@yahoo.com>
2020-07-16 22:05:41 -0400

    finished all tasks, index.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ Car.prototype.drive = function(distance) {
   if (this.tank > 0) {
       this.odometer += this.tank * this.milesPerGallon;
       this.tank -= this.tank;
-  if (this.tank == 0) {
+  if (this.tank === 0) {
      return `I ran out of fuel at ${this.odometer} miles!`;
   }; 
   };
